a93a6f8e3dc56b20968d227c1ed301cb0561cb53
jauzou <jauzou@qq.com>
2017-09-30 22:14:25 +0800

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { AsideComponent } from './admin/aside/aside.component';
+import { SectionComponent } from './admin/section/section.component';
+import { FooterComponent } from './admin/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    SectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
