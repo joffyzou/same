8f30d985320710a5b8bcf8331c158cb6772bac6f
jauzou <jauzou@qq.com>
2017-10-07 23:04:45 +0800

    aside

--- src/app/admin/header/header.component.ts
+++ src/app/admin/header/header.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css']
+	templateUrl: './header.component.html'
 })
 
 export class HeaderComponent {
