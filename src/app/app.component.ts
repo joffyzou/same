5a2eb2ab2207bc876c0a93a09881a05586a4ef89
jauzou <jauzou@qq.com>
2017-10-09 00:12:21 +0800

    add dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-	
+export class AppComponent implements OnInit {
+	ngOnInit(){
+		var headerH = 62;
+		var footerH = 41;
+		var section = document.getElementById("section");
+		var bodyH = document.body.clientHeight;
+		section.style.minHeight = bodyH - headerH - footerH +'px';
+		window.onresize = function(){
+			var bodyH = document.body.clientHeight;
+			section.style.minHeight = bodyH - headerH - footerH +'px';
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './admin/header/header.component';
 import { AsideComponent } from './admin/aside/aside.component';
-import { SectionComponent } from './admin/section/section.component';
 import { FooterComponent } from './admin/footer/footer.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 
 @NgModule({
@@ -13,8 +13,8 @@ import { FooterComponent } from './admin/footer/footer.component';
     AppComponent,
     HeaderComponent,
     AsideComponent,
-    SectionComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
